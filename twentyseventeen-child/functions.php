13125b0742b59ef65cec2b6cff9add5a80c08fc2
Tim Schaefer <ts+code@rcmd.org>
2018-05-13 18:49:40 +0200

    Update functions to use new font, add CSS for regular version of font.

--- twentyseventeen-child/functions.php
+++ twentyseventeen-child/functions.php
@@ -10,5 +10,21 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+// Do not use Google fonts remotely, enabling usage of the local version in fonts/.
+// This improves the privacy of website visitors, as requests to the Google fonts may allow tracking.
+function dequeue_unwanted_parent_theme_stuff() {
+    wp_dequeue_style('google-fonts');
+    wp_deregister_style('google-fonts');
+}
+
+// Prevent leaking of HTTP referer [sic] for browser's which support this.
+function add_privacy_headers(){
+    ?>
+    <meta name="referrer" content="no-referrer">
+    <?php
+}
+
+add_action('wp_head', 'add_privacy_headers');
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'dequeue_unwanted_parent_theme_stuff', 100);
 ?>
